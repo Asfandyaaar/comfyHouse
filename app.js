929ef7fae0b1637e6fadb136e559e0c56dfca1fd
Asfandyar <fandiyaar400@gmail.com>
2022-04-13 08:20:49 +0500

    comfyHouse project

---
+++ app.js
@@ -0,0 +1,25 @@
+const cartBtn = document.querySelector('.cart-btn');
+const closeCart = document.querySelector('#close-cart');
+const clearCart =document.querySelector('#clear-cart-btn');
+const cart = document.querySelector('#cart');
+const cartoverlay = document.querySelector('#cart-overlay');
+const cartItem =document.querySelector('#cart-item');
+const cartTotal =document.querySelector('#cart-total');
+const cartContent =document.querySelector('#cart-content');
+const productContainer =document.querySelector('#product-container');
+let mycart = [];
+
+// Getting the products
+class products{
+
+}
+
+//display products
+class UI{
+    
+}
+
+// Local Storage
+class storage{
+    
+}
